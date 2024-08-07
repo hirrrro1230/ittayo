610a2fe7f9d538e6cb21dd218192be0c7ac6955c
hirrro1230 <hirrrro1230@example.com>
2024-08-07 23:15:01 +0900

    ユーザー登録画面骨組み

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    // 登録画面を表示
+    public function create()
+    {
+        return view('users.create');
+    }
 }
