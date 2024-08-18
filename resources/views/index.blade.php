d9842cc808a76fb56c877a0928f1932450cd6daa
hirrro1230 <hirrrro1230@example.com>
2024-08-18 22:03:38 +0900

    レイアウト修正

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,12 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Ittayo</title>
-    </head>
-    <body>
-        <div>
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('アカウントをお持ちの方はこちら') }}
-            </a>
-        </div>
-        <div>
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
-                {{ __('新規登録はこちら') }}
-            </a>
-        </div>
-    </body>
-</html>
+<x-guest-layout>
+    <div>
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
+            {{ __('アカウントをお持ちの方はこちら') }}
+        </a>
+    </div>
+    <div>
+        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('register') }}">
+            {{ __('新規登録はこちら') }}
+        </a>
+    </div>
+</x-guest-layout>
